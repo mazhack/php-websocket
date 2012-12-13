66a623e76ed61e2b46bbe78fdcfb0a3d10dbdc87
Juan Enrique Escobar Robles <neblipedia@gmail.com>
2012-12-13 12:18:06 -0500

    formateo de codigo

--- lib/WebSocket/ConnectionWs.php
+++ lib/WebSocket/ConnectionWs.php
@@ -358,45 +358,45 @@ class ConnectionWs extends Connection {
         $this->close(1004);
         return false;
       }
-  		}
-  		elseif($payloadLength > 125)
-  		{
-  		  $payloadLengthBin = str_split(sprintf('%016b', $payloadLength), 8);
-  		  $frameHead[1] = ($masked === true) ? 254 : 126;
-  		  $frameHead[2] = bindec($payloadLengthBin[0]);
-  		  $frameHead[3] = bindec($payloadLengthBin[1]);
-  		}
-  		else
-  		{
-  		  $frameHead[1] = ($masked === true) ? $payloadLength + 128 : $payloadLength;
-  		}
+    }
+    elseif($payloadLength > 125)
+    {
+      $payloadLengthBin = str_split(sprintf('%016b', $payloadLength), 8);
+      $frameHead[1] = ($masked === true) ? 254 : 126;
+      $frameHead[2] = bindec($payloadLengthBin[0]);
+      $frameHead[3] = bindec($payloadLengthBin[1]);
+    }
+    else
+    {
+      $frameHead[1] = ($masked === true) ? $payloadLength + 128 : $payloadLength;
+    }
 
-  		// convert frame-head to string:
-  		foreach(array_keys($frameHead) as $i)
-  		{
-  		  $frameHead[$i] = chr($frameHead[$i]);
-  		}
-  		if($masked === true)
-  		{
-  		  // generate a random mask:
-  		  $mask = array();
-  		  for($i = 0; $i < 4; $i++)
-  		  {
-  		    $mask[$i] = chr(rand(0, 255));
-  		  }
+    // convert frame-head to string:
+    foreach(array_keys($frameHead) as $i)
+    {
+      $frameHead[$i] = chr($frameHead[$i]);
+    }
+    if($masked === true)
+    {
+      // generate a random mask:
+      $mask = array();
+      for($i = 0; $i < 4; $i++)
+      {
+        $mask[$i] = chr(rand(0, 255));
+      }
 
-  		  $frameHead = array_merge($frameHead, $mask);
-  		}
-  		$frame = implode('', $frameHead);
+      $frameHead = array_merge($frameHead, $mask);
+    }
+    $frame = implode('', $frameHead);
 
-  		// append payload to frame:
-  		$framePayload = array();
-  		for($i = 0; $i < $payloadLength; $i++)
-  		{
-  		  $frame .= ($masked === true) ? $payload[$i] ^ $mask[$i % 4] : $payload[$i];
-  		}
+    // append payload to frame:
+    $framePayload = array();
+    for($i = 0; $i < $payloadLength; $i++)
+    {
+      $frame .= ($masked === true) ? $payload[$i] ^ $mask[$i % 4] : $payload[$i];
+    }
 
-  		return $frame;
+    return $frame;
   }
 
   protected function hybi10Decode($data)
@@ -414,97 +414,96 @@ class ConnectionWs extends Connection {
     $payloadLength = ord($data[1]) & 127;
 
     // close connection if unmasked frame is received:
-    if($isMasked === false)
-    {
+    if($isMasked === false){
       $this->close(1002);
-  		}
+    }
 
-  		switch($opcode)
-  		{
-  		  // text frame:
-  		  case 1:
-  		    $decodedData['type'] = 'text';
-  		    break;
+    switch($opcode)
+    {
+      // text frame:
+      case 1:
+        $decodedData['type'] = 'text';
+        break;
 
-  		  case 2:
-  		    $decodedData['type'] = 'binary';
-  		    break;
+      case 2:
+        $decodedData['type'] = 'binary';
+        break;
 
-  		    // connection close frame:
-  		  case 8:
-  		    $decodedData['type'] = 'close';
-  		    break;
+        // connection close frame:
+      case 8:
+        $decodedData['type'] = 'close';
+        break;
 
-  		    // ping frame:
-  		  case 9:
-  		    $decodedData['type'] = 'ping';
-  		    break;
+        // ping frame:
+      case 9:
+        $decodedData['type'] = 'ping';
+        break;
 
-  		    // pong frame:
-  		  case 10:
-  		    $decodedData['type'] = 'pong';
-  		    break;
+        // pong frame:
+      case 10:
+        $decodedData['type'] = 'pong';
+        break;
 
-  		  default:
-  		    // Close connection on unknown opcode:
-  		    $this->close(1003);
-  		    break;
-  		}
+      default:
+        // Close connection on unknown opcode:
+        $this->close(1003);
+        break;
+    }
 
-  		if($payloadLength === 126)
-  		{
-  		  $mask = substr($data, 4, 4);
-  		  $payloadOffset = 8;
-  		  $dataLength = bindec(sprintf('%08b', ord($data[2])) . sprintf('%08b', ord($data[3]))) + $payloadOffset;
-  		}
-  		elseif($payloadLength === 127)
-  		{
-  		  $mask = substr($data, 10, 4);
-  		  $payloadOffset = 14;
-  		  $tmp = '';
-  		  for($i = 0; $i < 8; $i++)
-  		  {
-  		    $tmp .= sprintf('%08b', ord($data[$i+2]));
-  		  }
-  		  $dataLength = bindec($tmp) + $payloadOffset;
-  		  unset($tmp);
-  		}
-  		else
-  		{
-  		  $mask = substr($data, 2, 4);
-  		  $payloadOffset = 6;
-  		  $dataLength = $payloadLength + $payloadOffset;
-  		}
+    if($payloadLength === 126)
+    {
+      $mask = substr($data, 4, 4);
+      $payloadOffset = 8;
+      $dataLength = bindec(sprintf('%08b', ord($data[2])) . sprintf('%08b', ord($data[3]))) + $payloadOffset;
+    }
+    elseif($payloadLength === 127)
+    {
+      $mask = substr($data, 10, 4);
+      $payloadOffset = 14;
+      $tmp = '';
+      for($i = 0; $i < 8; $i++)
+      {
+        $tmp .= sprintf('%08b', ord($data[$i+2]));
+      }
+      $dataLength = bindec($tmp) + $payloadOffset;
+      unset($tmp);
+    }
+    else
+    {
+      $mask = substr($data, 2, 4);
+      $payloadOffset = 6;
+      $dataLength = $payloadLength + $payloadOffset;
+    }
 
-  		/**
-  		 * We have to check for large frames here. socket_recv cuts at 1024 bytes
-  		 * so if websocket-frame is > 1024 bytes we have to wait until whole
-  		 * data is transferd.
-  		 */
-  		if(strlen($data) < $dataLength)
-  		{
-  		  return false;
-  		}
+    /**
+     * We have to check for large frames here. socket_recv cuts at 1024 bytes
+     * so if websocket-frame is > 1024 bytes we have to wait until whole
+     * data is transferd.
+     */
+    if(strlen($data) < $dataLength)
+    {
+      return false;
+    }
 
-  		if($isMasked === true)
-  		{
-  		  for($i = $payloadOffset; $i < $dataLength; $i++)
-  		  {
-  		    $j = $i - $payloadOffset;
-  		    if(isset($data[$i]))
-  		    {
-  		      $unmaskedPayload .= $data[$i] ^ $mask[$j % 4];
-  		    }
-  		  }
-  		  $decodedData['payload'] = $unmaskedPayload;
-  		}
-  		else
-  		{
-  		  $payloadOffset = $payloadOffset - 4;
-  		  $decodedData['payload'] = substr($data, $payloadOffset);
-  		}
+    if($isMasked === true)
+    {
+      for($i = $payloadOffset; $i < $dataLength; $i++)
+      {
+        $j = $i - $payloadOffset;
+        if(isset($data[$i]))
+        {
+          $unmaskedPayload .= $data[$i] ^ $mask[$j % 4];
+        }
+      }
+      $decodedData['payload'] = $unmaskedPayload;
+    }
+    else
+    {
+      $payloadOffset = $payloadOffset - 4;
+      $decodedData['payload'] = substr($data, $payloadOffset);
+    }
 
-  		return $decodedData;
+    return $decodedData;
   }
 
   public function getClientIp()
